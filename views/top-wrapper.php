8f496e32a48979ceeaeabbd89cdee6738030814f
JWhoami <jerry.whoami@gmail.com>
2024-11-19 10:47:44 -0500

    remove toggle

--- views/top-wrapper.php
+++ views/top-wrapper.php
@@ -5,13 +5,6 @@ $currentPage = basename($_SERVER['PHP_SELF']);
 
 <nav class="sidebar" id="sidebar">
   <div class="nav-header">
-    <div id="menu-toggle">
-      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu">
-        <line x1="4" x2="20" y1="12" y2="12" />
-        <line x1="4" x2="20" y1="6" y2="6" />
-        <line x1="4" x2="20" y1="18" y2="18" />
-      </svg>
-    </div>
     <img
       class="logo"
       src="../uploads/logo-test.png"

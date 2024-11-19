06f01c1f59b94f9867ee4a5bfb0654af931ea722
JWhoami <jerry.whoami@gmail.com>
2024-11-18 21:55:54 -0500

    Ignore includes/config.php

--- includes/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');         // Cambia por tu usuario de la base de datos si es diferente
-define('DB_PASSWORD', 'Datawi123!');  // Cambia por tu contraseña de la base de datos
-define('DB_PORT', '3307');               //  por el puerto que estás usando si es diferente
-define('DB_NAME', 'campushubdb');   
-define('DB_CHARSET', 'utf8mb4');   
-?>

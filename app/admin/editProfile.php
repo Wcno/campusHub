3b43715ba2b14bbe9eb5c2c0c6d5e40559b82dcf
Eynarr <moraleseynar13@gmail.com>
2024-11-23 10:54:34 -0500

    Update editProfile.php

--- app/admin/editProfile.php
+++ app/admin/editProfile.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['user']['id'])) {
 }
 
 $userId = $_SESSION['user']['id'];
-$uploadDir = '../uploads/';
+$uploadDir = '/var/www/html/campusHub/uploads/';
 $webPath = '/campusHub/uploads/';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {

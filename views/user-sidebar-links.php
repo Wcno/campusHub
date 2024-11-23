a7e4b164ad02a00cd1adcb8d52af0cd8237bc003
Wifredo Cano <107496103+Wcno@users.noreply.github.com>
2024-11-23 12:18:14 -0500

    fixed bug hover sidebar users

--- views/user-sidebar-links.php
+++ views/user-sidebar-links.php
@@ -1,25 +1,25 @@
 <div class="nav-links">
-  <a
-    href="<?php echo baseUrl('/app/events/home') ?>"
-    class="sidebar-item <?php echo (currentRouteIncludes('/events') && !currentRouteIncludes('/home')) ? 'active' : '' ?>">
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house">
-      <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
-      <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-    </svg>
-    <span>Inicio</span>
-  </a>
+<a
+  href="<?php echo baseUrl('/app/events/home') ?>"
+  class="sidebar-item <?php echo currentRouteIncludes('/events/home') ? 'active' : '' ?>">
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house">
+    <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+    <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+  </svg>
+  <span>Inicio</span>
+</a>
 
-  <a
-    href="<?php echo baseUrl('/app/events/my-events') ?>"
-    class="sidebar-item <?php echo currentRouteIncludes('/my-events') ? 'active' : '' ?>">
-    <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar">
-      <path d="M8 2v4" />
-      <path d="M16 2v4" />
-      <rect width="18" height="18" x="3" y="4" rx="2" />
-      <path d="M3 10h18" />
-    </svg>
-    <span>Mis Eventos</span>
-  </a>
+<a
+  href="<?php echo baseUrl('/app/events/my-events') ?>"
+  class="sidebar-item <?php echo currentRouteIncludes('/my-events') ? 'active' : '' ?>">
+  <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar">
+    <path d="M8 2v4" />
+    <path d="M16 2v4" />
+    <rect width="18" height="18" x="3" y="4" rx="2" />
+    <path d="M3 10h18" />
+  </svg>
+  <span>Mis Eventos</span>
+</a>
 </div>
 
 <div>
